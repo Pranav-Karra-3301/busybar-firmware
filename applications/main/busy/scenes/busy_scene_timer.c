4a59f80f618679f2b8753f5d2ecf629328cff7e7
Georgii Surkov <37121527+gsurkov@users.noreply.github.com>
2025-02-17 18:14:09 +0000

    LVGL graphics (#34)
    * Implement basic LVGL on the back display
    
    * Add custom lvgl font library
    
    * Move oled display to drivers, give it a proper name
    
    * Delete oled_screen service (no longer relevant)
    
    * Implement basic LVGL on the front display
    
    * Add more fonts
    
    * Disable all gui apps for now
    
    * Add dynamic counter
    
    * Increase refresh period to 100 ms to show glitches
    
    * Remove useless line
    
    * VSYNC before frame load
    
    * Fix formatting
    
    * Display dynamic text on both screens
    
    * Rename lvgl_gui to lvgl
    
    * Remove old loader, rename loader_simple to loader
    
    * Disable Vsync after 10 display updates
    
    * Improve front display startup procedure
    
    * Add gui_lvgl and busy applications
    
    * Disable mono theme
    
    * Implement working input and menu
    
    * Increase animation speed
    
    * Implement scenes
    
    * Implement progress bar
    
    * Better menu alignment
    
    * Add more images
    
    * Add advanced events
    
    * Implement busy scene states
    
    * Move timer logic out of busy scene
    
    * Rename busy scene to timer
    
    * Implement cycle count
    
    * Implement pause overlay
    
    * Launch timer from main menu by button press
    
    * Implement static busy scene
    
    * Prevent state change when paused
    
    * Implement quit confirmation screen
    
    * Take the static busy scene into account
    
    * Improve update logic
    
    * Add a mock setup screen
    
    * Update submodules
    
    * Improve setup menu
    
    * Improve label formatting
    
    * Reset input group on delete
    
    * Implement somewhat working settings
    
    * Reduce default total time for convenience
    
    * Implement the Next state scene
    
    * Implement next state logic settings influence
    
    * Streamline scene data allocation
    
    * Save cycle time to a different variable
    
    * Rework input to use event driven mode, generalise for several input devices
    
    * Add separate input device for back and start buttons
    
    * Fix input handling for the variable item
    
    * Implement input workarounds for consistent behaviour
    
    * Adjust label
    
    * Add simple text on the back display
    
    * Add shutdown option
    
    * Add ludicrous speed mode
    
    * Rename variables, add new scene
    
    * Rearrange timer code
    
    * Implement autorestart session
    
    * Fix autorestart busy in some situations
    
    * Refactor busy timer events
    
    * Remove copypasta
    
    * fbt: fixes for excessive submodule updates
    
    ---------
    
    Co-authored-by: nminaylov <nm29719@gmail.com>
    Co-authored-by: hedger <hedger@nanode.su>

---
+++ applications/main/busy/scenes/busy_scene_timer.c
@@ -0,0 +1,188 @@
+#include "busy_scene_timer.h"
+
+extern const lv_image_dsc_t I_busy_39x14;
+extern const lv_image_dsc_t I_rest_39x14;
+extern const lv_image_dsc_t I_long_rest_39x14;
+extern const lv_image_dsc_t I_pause_5x5;
+
+typedef struct {
+    lv_obj_t* main_image;
+    lv_obj_t* time_label;
+    lv_obj_t* info_label;
+    lv_obj_t* time_bar;
+    lv_obj_t* overlay;
+    lv_obj_t* overlay_image;
+    BusyTimerState timer_state;
+} BusySceneTimer;
+
+static void busy_scene_timer_state_update(BusySceneTimer* data) {
+    const lv_image_dsc_t* main_image_dsc;
+    uint32_t bar_color_main;
+    uint32_t bar_color_indicator;
+
+    // TODO: Use colours from theme
+    if(data->timer_state == BusyTimerStateBusy) {
+        main_image_dsc = &I_busy_39x14;
+        bar_color_main = 0x4A0000;
+        bar_color_indicator = 0xFF0000;
+
+    } else if(data->timer_state == BusyTimerStateRest) {
+        main_image_dsc = &I_rest_39x14;
+        bar_color_main = 0x033013;
+        bar_color_indicator = 0x13F562;
+
+    } else if(data->timer_state == BusyTimerStateLongRest) {
+        main_image_dsc = &I_long_rest_39x14;
+        bar_color_main = 0x081631;
+        bar_color_indicator = 0x2C72FA;
+
+    } else {
+        furi_crash("Invalid state");
+    }
+
+    lv_image_set_src(data->main_image, main_image_dsc);
+    lv_obj_set_style_bg_color(data->time_bar, lv_color_hex(bar_color_main), LV_PART_MAIN);
+    lv_obj_set_style_bg_color(
+        data->time_bar, lv_color_hex(bar_color_indicator), LV_PART_INDICATOR);
+}
+
+static void busy_scene_timer_update(BusyApp* instance) {
+    BusySceneTimer* data = busy_get_current_scene_data(instance);
+
+    gui_lvgl_acquire(instance->gui);
+
+    if(data->timer_state != instance->state) {
+        data->timer_state = instance->state;
+        busy_scene_timer_state_update(data);
+    }
+
+    const uint32_t minutes = S_TO_M(instance->interval_time_left_s);
+    const uint32_t seconds = S_TO_R(instance->interval_time_left_s);
+    const uint32_t percent = (instance->interval_time_left_s * 100) / instance->interval_time_s;
+
+    lv_label_set_text_fmt(data->time_label, "%02lu:%02lu", minutes, seconds);
+    lv_bar_set_value(data->time_bar, percent, LV_ANIM_OFF);
+
+    if(data->timer_state == BusyTimerStateBusy) {
+        lv_label_set_text_fmt(instance->back_label, "BUSY: %02lu:%02lu", minutes, seconds);
+    } else if(data->timer_state == BusyTimerStateRest) {
+        lv_label_set_text_fmt(instance->back_label, "REST: %02lu:%02lu", minutes, seconds);
+    } else if(data->timer_state == BusyTimerStateLongRest) {
+        lv_label_set_text_fmt(instance->back_label, "LONG REST: %02lu:%02lu", minutes, seconds);
+    }
+
+    gui_lvgl_release(instance->gui);
+}
+
+static void busy_scene_timer_toggle_pause_overlay(BusyApp* instance) {
+    BusySceneTimer* data = busy_get_current_scene_data(instance);
+
+    gui_lvgl_acquire(instance->gui);
+
+    if(data->overlay == NULL) {
+        lv_obj_t* top = gui_lvgl_get_layer(instance->gui, GuiDisplayIdFront, GuiLayerIdTop);
+
+        data->overlay = lv_obj_create(top);
+        lv_obj_set_size(data->overlay, lv_obj_get_width(top), lv_obj_get_height(top));
+        lv_obj_set_style_opa(data->overlay, LV_OPA_70, LV_PART_MAIN);
+
+        data->overlay_image = lv_image_create(top);
+        lv_image_set_src(data->overlay_image, &I_pause_5x5);
+        lv_obj_set_pos(data->overlay_image, 33, 5);
+
+    } else {
+        lv_obj_delete(data->overlay);
+        lv_obj_delete(data->overlay_image);
+
+        data->overlay = NULL;
+        data->overlay_image = NULL;
+    }
+
+    gui_lvgl_release(instance->gui);
+}
+
+static void busy_scene_timer_on_enter(void* context) {
+    BusyApp* instance = context;
+    BusySceneTimer* data = busy_get_current_scene_data(instance);
+
+    gui_lvgl_acquire(instance->gui);
+
+    lv_obj_t* active = gui_lvgl_get_layer(instance->gui, GuiDisplayIdFront, GuiLayerIdActive);
+
+    data->main_image = lv_image_create(active);
+
+    data->time_label = lv_label_create(active);
+    lv_obj_set_style_text_font(data->time_label, &lv_font_pixel_operator_8, LV_PART_MAIN);
+    // TODO: Implement in theme
+    lv_obj_set_style_text_color(data->time_label, lv_color_white(), LV_PART_MAIN);
+    lv_obj_set_pos(data->time_label, 41, 1);
+
+    data->info_label = lv_label_create(active);
+    lv_label_set_text(data->info_label, "LEFT");
+    lv_obj_set_style_text_font(data->info_label, &lv_font_tiny_6, LV_PART_MAIN);
+    // TODO: Implement in theme
+    lv_obj_set_style_text_color(data->info_label, lv_color_white(), LV_PART_MAIN);
+    lv_obj_set_pos(data->info_label, 41, 10);
+
+    data->time_bar = lv_bar_create(active);
+    lv_obj_set_pos(data->time_bar, 1, 15);
+    lv_obj_set_size(data->time_bar, lv_obj_get_width(active) - 2, 1);
+
+    gui_lvgl_release(instance->gui);
+
+    busy_scene_timer_update(instance);
+}
+
+static void busy_scene_timer_on_exit(void* context) {
+    BusyApp* instance = context;
+    BusySceneTimer* data = busy_get_current_scene_data(instance);
+
+    gui_lvgl_acquire(instance->gui);
+
+    lv_obj_delete(data->main_image);
+    lv_obj_delete(data->time_label);
+    lv_obj_delete(data->info_label);
+    lv_obj_delete(data->time_bar);
+
+    if(data->overlay) {
+        lv_obj_delete(data->overlay);
+        lv_obj_delete(data->overlay_image);
+    }
+
+    gui_lvgl_release(instance->gui);
+}
+
+static void busy_scene_timer_on_event(const BusyEvent* event, void* context) {
+    BusyApp* instance = context;
+
+    if(event->type == BusyEventTypeStart) {
+        busy_scene_timer_toggle_pause_overlay(instance);
+        busy_timer_toggle(instance);
+
+    } else if(event->type == BusyEventTypeBack) {
+        if(busy_timer_is_running(instance)) {
+            busy_switch_to_scene(instance, BusyAppSceneIdQuit);
+        }
+
+    } else if(event->type == BusyEventTypeOk) {
+        if(busy_timer_is_running(instance)) {
+            busy_timer_next_state(instance, true);
+        }
+
+    } else if(event->type == BusyEventTypeCustom) {
+        if(event->custom_value == BusyCustomEventUpdate) {
+            busy_scene_timer_update(instance);
+        } else if(event->custom_value == BusyCustomEventIntervalEnd) {
+            busy_switch_to_scene(instance, BusyAppSceneIdNext);
+        } else if(event->custom_value == BusyCustomEventSessionEnd) {
+            busy_switch_to_scene(instance, BusyAppSceneIdRestart);
+        }
+    }
+}
+
+const BusyAppScene busy_scene_timer = {
+    .on_enter = busy_scene_timer_on_enter,
+    .on_exit = busy_scene_timer_on_exit,
+    .on_event = busy_scene_timer_on_event,
+    .data_size = sizeof(BusySceneTimer),
+};
