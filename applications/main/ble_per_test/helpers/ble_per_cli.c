09f528ed8177e41bb9992f3386fc91d9c23037c2
Skorpionm <85568270+Skorpionm@users.noreply.github.com>
2025-03-26 16:58:13 +0400

    WiFi_PER_Test GUI (#74)
    * init_app
    
    * add var_item_list_add_selector_key_value
    
    * app init
    
    * add set setting
    
    * transfer settings to 917
    
    * fix var_item_set_value
    
    * add var_item_set_value_key_value
    
    * set all setting
    
    * fix gui
    
    * show status
    
    * fix show rssi
    
    * add WiFi_PER_Test GUI, fix syntax BLE_PER_Test app
    
    * ordered apps menu
    
    ---------
    
    Co-authored-by: hedger <hedger@nanode.su>

--- applications/main/ble_per_test/helpers/ble_per_cli.c
+++ applications/main/ble_per_test/helpers/ble_per_cli.c
@@ -13,7 +13,7 @@ typedef struct {
     uint8_t rx_data[CLI_BUFFER_SIZE];
     FuriThread* thread;
     FuriString* rx_msg;
-    BlePerTest* app_hendle;
+    BlePerTest* app_handle;
 } CliCommandSlCli;
 
 typedef enum {
@@ -160,7 +160,7 @@ static int32_t ble_per_cli_worker_thread(void* context) {
                                 ble_per_cli_stats[y].value = arg_int32;
 
                                 ble_per_test_update(
-                                    instance->app_hendle,
+                                    instance->app_handle,
                                     ble_per_cli_stats[BlePerCliStatsCmdTypeTxDones].value,
                                     ble_per_cli_stats[BlePerCliStatsCmdTypeCrcFailCnt].value,
                                     ble_per_cli_stats[BlePerCliStatsCmdTypeCrcPassCnt].value,
@@ -187,14 +187,14 @@ static int32_t ble_per_cli_worker_thread(void* context) {
     return 0;
 }
 
-void ble_per_cli_start(BlePerTest* app_hendle, BlePerCliSettings settings) {
+void ble_per_cli_start(BlePerTest* app_handle, BlePerCliSettings settings) {
     UNUSED(settings);
     if(ble_per_cli_instance != NULL) {
         return;
     }
 
     ble_per_cli_instance = malloc(sizeof(CliCommandSlCli));
-    ble_per_cli_instance->app_hendle = app_hendle;
+    ble_per_cli_instance->app_handle = app_handle;
     for(size_t i = 0; i < BlePerCliStatsCmdMax; i++) {
         ble_per_cli_stats[i].value = 0;
     }
