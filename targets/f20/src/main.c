fe07d647de6ce771a4815617ccca05962ad4687f
SkorP <skorpionm@yandex.ru>
2025-04-11 15:53:46 +0400

    U5 set STANDBY

--- targets/f20/src/main.c
+++ targets/f20/src/main.c
@@ -2,6 +2,12 @@
 #include <furi_hal.h>
 #include <flipper.h>
 
+#include <stm32u5xx_ll_cortex.h>
+#include <stm32u5xx_ll_system.h>
+#include <stm32u5xx_ll_pwr.h>
+#include <stm32u5xx_ll_utils.h>
+#include <furi_hal_clock.h>
+
 #define TAG "Main"
 
 int32_t init_task(void* context) {
@@ -12,6 +18,25 @@ int32_t init_task(void* context) {
 
     // Set the UART for logging output
     furi_hal_serial_control_set_logging_config(FuriHalSerialIdUsart6, 230400);
+
+
+    furi_log_puts("Flipper Init\r\n");
+    furi_delay_ms(5000);
+    furi_log_puts("Flipper Init Low Power 1\r\n");
+    furi_hal_gpio_init(&gpio_oled_vcc_en, GpioModeOutputPushPull, GpioPullNo, GpioSpeedLow);
+    furi_hal_gpio_init(&gpio_led_power_en, GpioModeOutputPushPull, GpioPullNo, GpioSpeedLow);
+    furi_hal_gpio_write(&gpio_oled_vcc_en, 0);
+    furi_hal_gpio_write(&gpio_led_power_en, 0);
+
+    //furi_hal_clock_suspend_tick();
+    LL_PWR_SetPowerMode(PWR_CR1_LPMS_2); //STOP 0
+    LL_LPM_EnableDeepSleep();
+    __WFI();
+    LL_LPM_EnableSleep();
+
+    //furi_hal_clock_resume_tick();
+    furi_log_puts("Flipper Init Stop 0\r\n");
+    furi_delay_ms(5000);
 
     // Init flipper
     flipper_init();
@@ -28,6 +53,14 @@ int main(void) {
 
     // Flipper critical FURI HAL
     furi_hal_init_early();
+
+    // for(uint32_t i = 0; i < 1600000; i++) {
+    //     __asm volatile("nop");
+    // }
+    // LL_PWR_SetPowerMode(0); //STOP 0
+    // LL_LPM_EnableDeepSleep();
+    // __WFI();
+    // LL_LPM_EnableSleep();
 
     FuriThread* main_thread = furi_thread_alloc_ex("Init", 4096, init_task, NULL);
 
