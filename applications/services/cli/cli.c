d69a67e07e19041f9f81aa2e0abbfdc9c1f85963
Georgii Surkov <37121527+gsurkov@users.noreply.github.com>
2025-06-13 21:22:02 +0100

    Busy App Refactoring Pt.4 (#164)
    * Busy: reset state variables on scene exit
    
    * FuriHalPwm: only enable GPIO when pwm is enabled
    
    * StatusLights: add Off preset
    
    * Busy: only enable status lights in timer mode
    
    * Busy: add done transitions
    
    * Busy: Rework fast mode
    
    * Busy: add blur and dim effects to SnapImage
    
    * Busy: implement animated pause screen
    
    * Busy: add blinking pause animation
    
    * Busy: exit from pause with back button
    
    * Busy: fix erroneous skip behaviour
    
    * Busy: adjust text fall animation
    
    * Busy: add ability to skip/exit from overview scene + potential crash fixes
    
    * Busy: cancel previous transition before starting a new one
    
    * Busy: divide various presets to separate file
    
    * Busy: remove empty line
    
    * Busy: reset snap image position
    
    * Busy: refactor NavHeader to NavStack
    
    * Busy: use image recolouring
    
    * Busy: more consistent switching of persistent views
    
    * Busy: move a file
    
    * Gui: implement missing Menu methods
    
    * Gui: limit Menu sublabel width on Front display
    
    * Busy: add stub setting for timer mode
    
    * Gui: hide Submenu cursor on Back display
    
    * Busy: stub implementation for other timer modes
    
    * Gui: fix flex layout spacing
    
    * Busy: improve widget positioning
    
    * Busy: implement morphing infinite timer to simple
    
    * Busy: hide timer card details not on timer scene
    
    * Busy: add skip transition, refactor transition names
    
    * Busy: implement infinite to simple timer transition
    
    * Busy: improve transition timings and curves
    
    * UsbNetwork: fix vibe coding silliness
    
    * Busy: add settings support
    
    * Busy: better naming for Demo mode
    
    * Gui: improve AnimImage state tracking
    
    * Busy: update progress bar only when it's visible
    
    * Busy: fix pause overlay when user mashes start/pause button
    
    * Busy: remove unused scenes
    
    * Busy: add version field to settings file
    
    * Gui: generate a VarItem event on each change
    
    * Busy: hide irrelevant settings
    
    * Busy: refactor item hiding/showing code
    
    * Busy: fix typo
    
    * Busy: add doxygen comments
    
    * Busy: refactor transition overlay
    
    * Gui: implement anim_image_rewind()
    
    * Busy: implement ProgressBar presets
    
    * Busy: separate time setting for Simple timer
    
    * VarItemList: dim arrows if the value is at its bounds
    
    * Busy: fix memory leak
    
    * VarItemList: use small case
    
    * VarItemList: draw bigger arrows on Back display
    
    * FuriHal: replace furi_hal_rtc with furi_hal_nvm functions
    
    * Revert "FuriHal: replace furi_hal_rtc with furi_hal_nvm functions"
    
    This reverts commit 5f68d2bfc1e0b16973d1d452c2ef0f2b9e8ce237.
    
    * FuriHalNvm: make it compile
    
    * Busy: fix formatting
    
    * Busy: update icon
    
    * Busy: allow skipping from the progress scene
    
    ---------
    
    Co-authored-by: hedger <hedger@users.noreply.github.com>
    Co-authored-by: hedger <hedger@nanode.su>

--- applications/services/cli/cli.c
+++ applications/services/cli/cli.c
@@ -476,7 +476,7 @@ int32_t cli_srv(void* p) {
     cli_session_open(cli, &cli_session);
 
     //TODO: Implement furi_hal_nvm_get_boot_mode
-    // if(furi_hal_rtc_get_boot_mode() == FuriHalNvmBootModeNormal) {
+    // if(furi_hal_nvm_get_boot_mode() == FuriHalRtcBootModeNormal) {
     //     cli_session_open(cli, &cli_vcp);
     // } else {
     //     FURI_LOG_W(TAG, "Skipping start in special boot mode");
