d1c2d8688e0de13b03f7abf6d630c92827a571a3
RebornedBrain <RebornedBrain@gmail.com>
2025-07-30 11:04:58 +0300

    Some raw work on base  ble service. Now it is able to self-init all services and configure same part on 917

--- applications/services/ble/ble.c
+++ applications/services/ble/ble.c
@@ -1,5 +1,6 @@
 #include "ble.h"
 #include "ble_common.h"
+#include "ble_service.h"
 
 #include <intercom/intercom.h>
 #include <api_lock.h>
@@ -7,8 +8,15 @@
 #define TAG "BLE"
 
 typedef enum {
+    BleStateTypeIdle,
+    BleStateTypeIniting,
+    BleStateTypeReady,
+} BleStateType;
+
+typedef enum {
+    BleEventTypeInit,
     BleEventTypeIncomingMessage,
-    BleEventTypeFrameReceived
+    BleEventTypeFrameReceived,
 } BleEventType;
 
 typedef struct {
@@ -21,6 +29,11 @@ struct Ble {
     FuriSemaphore* access_semaphore;
     FuriEventLoop* event_loop;
     Intercom* intercom;
+
+    BleServiceObject* services[3];
+    BleIntercomServiceIndex pending_service_index;
+    BleStateType state;
+
     BleIntercomFrameGeneric frame;
     BleMessage* current_message;
 };
@@ -38,43 +51,59 @@ void ble_custom_event_callback(uint32_t events, void* context) {
             instance->frame.header.data_size = 0;
 
             size_t data_size = instance->frame.header.data_size + sizeof(BleIntercomFrameHeader);
-            //    sizeof(instance->frame.header.type) +
-            //    sizeof(instance->frame.header.data_size) +
-            //    sizeof(instance->frame.header.char_index);
             size_t tx_size = intercom_tx(
                 instance->intercom, IntercomChannelBle, &instance->frame, data_size, 100);
 
-            furi_assert(data_size == tx_size);
-        } else if(instance->current_message->type == BleRequestTypeInit) {
-            BleIntercomFrameServiceConfig* frd = (BleIntercomFrameServiceConfig*)&instance->frame;
-            frd->header.type = BleRequestTypeInit;
-            frd->header.service_index = BleIntercomServiceIndexDeviceInfo;
-            frd->header.data_size = sizeof(BleCharSize) * 3 + 1;
-
-            frd->char_count = 3;
-            frd->chars_config[0].intercom_index = BleSrvDeviceInfoCharacterIndexSerialNumber;
-            frd->chars_config[0].data_size = 8;
-
-            frd->chars_config[1].intercom_index = BleSrvDeviceInfoCharacterIndexHardwareRevision;
-            frd->chars_config[1].data_size = 5;
-
-            frd->chars_config[2].intercom_index = BleSrvDeviceInfoCharacterIndexSoftwareRevision;
-            frd->chars_config[2].data_size = 15;
-
-            size_t data_size = frd->header.data_size + sizeof(BleIntercomFrameHeader);
-            size_t tx_size = intercom_tx(
-                instance->intercom, IntercomChannelBle, &instance->frame, data_size, 100);
             furi_assert(data_size == tx_size);
         } else
             BLE_LOG_W("Wrong message type");
+
     } else if(events == BleEventTypeFrameReceived) {
         BLE_LOG_I("Frame received");
-        BleMessage* message = instance->current_message;
-        message->result = (instance->frame.header.type == BleRequestTypeEnable) ||
-                          (instance->frame.header.type == BleRequestTypeDisable) ||
-                          (instance->frame.header.type == BleRequestTypeInit);
-        api_lock_unlock(message->lock);
-        furi_semaphore_release(instance->access_semaphore);
+        if(instance->frame.header.type == BleRequestTypeWrite) {
+            BleServiceObject* service = instance->services[instance->pending_service_index];
+            if(service->desc->on_response) {
+                service->desc->on_response(service, &instance->frame);
+            }
+        } else if((instance->frame.header.type == BleRequestTypeInit)) {
+            if(instance->frame.header.service_index == instance->pending_service_index) {
+                BleServiceObject* service = instance->services[instance->pending_service_index];
+                if(service->desc->on_response) {
+                    service->desc->on_response(service, &instance->frame);
+                }
+
+                // instance->pending_service_index++;
+                if(instance->pending_service_index == 3) {
+                    instance->state = BleStateTypeReady;
+                    BLE_LOG_W("Init done");
+                }
+                //update service state from ble_services
+                // if(instance->state != BleStateTypeReady) {
+                //     furi_event_loop_set_custom_event(instance->event_loop, BleEventTypeInit);
+                // }
+
+            } else
+                BLE_LOG_W("Service index not match");
+        } else {
+            if(instance->current_message) {
+                BleMessage* message = instance->current_message;
+                message->result = (instance->frame.header.type == BleRequestTypeEnable) ||
+                                  (instance->frame.header.type == BleRequestTypeDisable);
+
+                instance->current_message = NULL;
+                api_lock_unlock(message->lock);
+            }
+            furi_semaphore_release(instance->access_semaphore);
+        }
+        // furi_semaphore_release(instance->access_semaphore);
+    } else if(events == BleEventTypeInit) {
+        bool result = false;
+        do {
+            uint16_t index = instance->pending_service_index;
+            BleServiceObject* service = instance->services[index];
+            result = ble_service_common_init(service, &instance->frame);
+            if(!result) instance->pending_service_index++;
+        } while(!result);
     }
 }
 
@@ -85,6 +114,15 @@ static void ble_backend_intercom_rx_callback(const void* data, size_t data_size,
     Ble* instance = context;
     memcpy(&instance->frame, data, data_size);
     furi_event_loop_set_custom_event(instance->event_loop, BleEventTypeFrameReceived);
+}
+
+static void ble_event_loop_on_start(void* context) {
+    BLE_LOG_W("ble_event_loop_on_start");
+    Ble* ble = context;
+    ble->pending_service_index = 0;
+    ble->state = BleStateTypeIniting;
+    furi_delay_ms(10000);
+    furi_event_loop_set_custom_event(ble->event_loop, BleEventTypeInit);
 }
 
 static Ble* ble_alloc() {
@@ -98,9 +136,16 @@ static Ble* ble_alloc() {
     furi_event_loop_set_custom_event_callback(
         instance->event_loop, ble_custom_event_callback, instance);
 
+    furi_event_loop_pend_callback(instance->event_loop, ble_event_loop_on_start, instance);
+
     instance->intercom = furi_record_open(RECORD_INTERCOM);
     intercom_set_rx_callback(
         instance->intercom, IntercomChannelBle, ble_backend_intercom_rx_callback, instance);
+
+    for(size_t i = 0; i < 3; i++) {
+        instance->services[i] = ble_worker_create_service(
+            &service_config[i], instance->access_semaphore, instance->intercom);
+    }
 
     furi_record_create(RECORD_BLE, instance);
 
@@ -131,14 +176,8 @@ static void ble_send_message(Ble* instance, BleMessage* message) {
 bool ble_start(Ble* ble) {
     BleMessage msg;
 
-    msg.type = BleRequestTypeInit;
+    msg.type = BleRequestTypeEnable;
     ble_send_message(ble, &msg);
-
-    if(msg.result) {
-        memset(&msg, 0, sizeof(BleMessage));
-        msg.type = BleRequestTypeEnable;
-        ble_send_message(ble, &msg);
-    }
     return msg.result;
 }
 
