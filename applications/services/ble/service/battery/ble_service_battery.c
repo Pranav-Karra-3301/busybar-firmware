528702959099449f527a5d9d788f416f706ba32a
RebornedBrain <RebornedBrain@gmail.com>
2025-08-05 13:06:00 +0300

    Init logic for battery service added

--- applications/services/ble/service/battery/ble_service_battery.c
+++ applications/services/ble/service/battery/ble_service_battery.c
@@ -1,6 +1,6 @@
 #include "ble_service_battery.h"
 
-#include <stdint.h>
+#include "../ble_service_i.h"
 
 #define TAG "BleBattery"
 
@@ -9,18 +9,59 @@ typedef enum {
     BleSrvBatteryCharacterIndexBatteryStatus,
 } BleSrvBatteryCharacterIndex;
 
+typedef union {
+    struct {
+        uint8_t battery_present         : 1;
+        uint8_t ext_source_present      : 2;
+        uint8_t wireless_source_present : 2;
+        uint8_t battery_charge_state    : 2;
+        uint8_t battery_charge_level    : 2;
+        uint8_t charging_type           : 3;
+        uint8_t charging_fault_reason   : 3;
+        uint8_t rfu                     : 1;
+    } fields;
+    uint16_t value;
+} BatteryPowerState;
+
 typedef struct {
     uint8_t flags;
-    uint16_t power_state;
-    uint8_t battery_level;
-} FURI_PACKED BatteryStatusInfo;
+    BatteryPowerState state;
+} /* FURI_PACKED */ BatteryStatusInfo;
 
-static bool ble_service_battery_init(void* context) {
-    furi_assert(context);
+static bool ble_service_battery_init(void* object) {
+    furi_assert(object);
 
     BLE_LOG_W("battery_init");
 
+    BleServiceObject* instance = object;
+
+    uint8_t* battery_level = malloc(sizeof(uint8_t));
+    *battery_level = 56;
+    instance->chars[BleSrvBatteryCharacterIndexBatteryLevel]->data = battery_level;
+    instance->chars[BleSrvBatteryCharacterIndexBatteryLevel]->data_size = sizeof(uint8_t);
+
+    BatteryStatusInfo* battery_status = malloc(sizeof(BatteryStatusInfo));
+
+    battery_status->flags = 0;
+    battery_status->state.fields.battery_present = 1;
+    battery_status->state.fields.ext_source_present = 0;
+    battery_status->state.fields.wireless_source_present = 0;
+    battery_status->state.fields.battery_charge_state = 2;
+    battery_status->state.fields.battery_charge_level = 1;
+    battery_status->state.fields.charging_type = 0;
+    battery_status->state.fields.charging_fault_reason = 0;
+
+    instance->chars[BleSrvBatteryCharacterIndexBatteryStatus]->data = battery_status;
+    instance->chars[BleSrvBatteryCharacterIndexBatteryStatus]->data_size =
+        sizeof(BatteryStatusInfo);
+
     return true;
+}
+
+static const uint8_t* battery_character_get_data(void* obj) {
+    furi_assert(obj);
+    BleCharacteristicObject* instance = obj;
+    return instance->data;
 }
 
 //==========================================================
@@ -29,6 +70,7 @@ static const BleCharacteristicDescriptor battery_service_characteristics[] = {
         .intercom_index = BleSrvBatteryCharacterIndexBatteryLevel,
         .name = "Battery Level",
         .data_size = sizeof(uint8_t),
+        .get_data = battery_character_get_data,
 #if defined(SI917)
         .uuid = {.Char_UUID_16 = 0x2A19},
         .uuid_size = 2,
@@ -39,6 +81,7 @@ static const BleCharacteristicDescriptor battery_service_characteristics[] = {
         .intercom_index = BleSrvBatteryCharacterIndexBatteryStatus,
         .name = "Battery Status",
         .data_size = sizeof(BatteryStatusInfo),
+        .get_data = battery_character_get_data,
 #if defined(SI917)
         .uuid = {.Char_UUID_16 = 0x2BED},
         .uuid_size = 2,
