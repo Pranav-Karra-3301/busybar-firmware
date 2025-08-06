3e40dcf1655c295066dda1fef5babb146a76dd3d
RebornedBrain <RebornedBrain@gmail.com>
2025-08-06 13:42:16 +0300

    Added raw ble_service init logic for 917

--- applications/services/ble/service/target/ble_service_target_917.c
+++ applications/services/ble/service/target/ble_service_target_917.c
@@ -1,36 +1,40 @@
 #include "ble_service_target.h"
+#include "../../worker/ble_worker.h"
 
 bool ble_service_target_init(BleServiceObject* instance) {
     FURI_LOG_I(instance->desc->name, "target_init_917");
 
     BleServiceState state = BleServiceStateReady;
 
-    FuriString* buf = furi_string_alloc();
-
     const BleIntercomFrameServiceConfig* frame =
         (BleIntercomFrameServiceConfig*)instance->frame_buf;
+
+    // if(instance->desc->init(instance))
 
     const BleServiceInitConfig* service_config = &frame->service_init;
     FURI_LOG_I(instance->desc->name, "Config char_count: %d", service_config->char_count);
     uint8_t offset = 0;
+
     for(size_t i = 0; i < service_config->char_count; i++) {
+        //char->handle = ble_worker_register_characteristic(uiid, properties, data, data_size)
+
         const BleCharacteristicInit* char_init =
             (BleCharacteristicInit*)((uint8_t*)service_config->chars_config + offset);
-
         size_t data_size = char_init->header.data_size;
-        FURI_LOG_I(instance->desc->name, "Char data_size: %d", data_size);
 
-        furi_string_reset(buf);
-        for(size_t j = 0; j < data_size; j++) {
-            furi_string_cat_printf(buf, "%02X", char_init->data[j]);
-        }
-        FURI_LOG_I(instance->desc->name, "Data: %s", furi_string_get_cstr(buf));
+        FURI_LOG_I(
+            instance->desc->name, "Char %d data_size: %d", char_init->header.index, data_size);
+        BleCharacteristicObject* ch = instance->chars[char_init->header.index];
+        ch->data_size = char_init->header.data_size;
+        ch->data = malloc(ch->data_size);
+        memcpy(ch->data, char_init->data, data_size);
 
         offset += (data_size + sizeof(BleCharacteristicInitHeader));
     }
-    furi_string_free(buf);
 
-    ble_service_switch_state(instance, state);
+    if(ble_worker_register_service(instance)) {
+        ble_service_switch_state(instance, state);
+    }
 
     ble_service_prepare_frame(
         instance, BleIntercomFrameTypeResponse, BleCommandServiceInit, 0, NULL);
