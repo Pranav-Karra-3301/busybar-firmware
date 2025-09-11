ac1a5482aa4c35bb3bc8d21e1567a4bd26661bd8
RebornedBrain <RebornedBrain@gmail.com>
2025-09-11 18:13:53 +0300

    Nordic uart characteristics now have indicate property

--- applications/services/ble/service/uart/ble_service_uart.c
+++ applications/services/ble/service/uart/ble_service_uart.c
@@ -50,7 +50,7 @@ static const BleCharacteristicDescriptor nordic_uart_service_characteristics[] =
 #if defined(SI917)
         .uuid = {.Char_UUID_128 = UART_RX_CHAR_UUID},
         .uuid_size = 16,
-        .char_properties = BLE_ATT_PROPERTY_READ | BLE_ATT_PROPERTY_WRITE,
+        .char_properties = BLE_ATT_PROPERTY_WRITE | BLE_ATT_PROPERTY_INDICATE,
 #endif
     },
     {
@@ -60,7 +60,7 @@ static const BleCharacteristicDescriptor nordic_uart_service_characteristics[] =
 #if defined(SI917)
         .uuid = {.Char_UUID_128 = UART_TX_CHAR_UUID},
         .uuid_size = 16,
-        .char_properties = BLE_ATT_PROPERTY_READ | BLE_ATT_PROPERTY_NOTIFY,
+        .char_properties = BLE_ATT_PROPERTY_READ | BLE_ATT_PROPERTY_INDICATE,
 #endif
     },
 };
