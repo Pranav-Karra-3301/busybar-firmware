bc1c0fc769524773e5561eaa80c20e24a37179ad
Anna Antonenko <portasynthinca3@gmail.com>
2025-10-06 18:43:34 +0400

    [FW-260] Matter commissioning (#262)
    * matter: use chip names instead of target names
    
    * gui: add QRCode module
    
    * gui: scene_manager_replace_current_scene
    
    * lvgl: enable qr_code
    
    * matter: commissioning control and statuses
    
    * matter_cli: commissioning control and statuses
    
    * settings: matter commissioning
    
    * wifi: fix typo
    
    * gui: flex_layout_set_align
    
    * label: label_set_color, label_set_font
    
    * qr_code: add calling order notices
    
    * rect: add
    
    * lvgl_addons: add "-" to ark_numerals_condensed
    
    * lvgl_addons: ark 10px: increase width of space char
    
    * lvgl_addons: add ark 12px
    
    * lvgl_addons: add styling for rect widget
    
    * settings: enhanced matter commissioning flow
    
    * settings: use actual help url
    
    * scripts: correct small typo
    
    * settings, lvgl_addons, gui: completely refactor matter settings scenes
    
    * settings, matter: detect number of commissioned fabrics
    
    * gui: revert label color and font methods
    
    * settings, lvgl_addons: move styles for matter_code_view from lvgl_addons to settings
    
    * matter_ext: silence -Wpsabi
    
    * matter_cli: add commissioning status query command
    
    * settings, lvgl_addons: format code
    
    * settings: adapt to new wifi api, more robust wifi handling
    
    * settings: restructure wifi logic, fix navbar popping
    
    * settings: remove unnecessary ext_evt_queue
    
    * matter_f64: react to fabric removed events
    
    * menu: fix alignment collateral damage from changed font
    
    * scripts: add executable attribute
    
    * lvgl_addons: update menu style
    
    ---------
    
    Co-authored-by: Georgii Surkov <37121527+gsurkov@users.noreply.github.com>

--- applications/system/settings/settings.h
+++ applications/system/settings/settings.h
@@ -6,15 +6,24 @@
 #include <audio/audio.h>
 #include <back_display/back_display.h>
 #include <front_display/front_display.h>
+#include <matter/matter.h>
 
 #include <gui/scene_manager.h>
 #include <gui/modules/nav_bar.h>
 #include <gui/modules/flex_layout.h>
 
+#include "helpers/wifi_poller.h"
+
 typedef enum {
     SettingsCustomEventAboutToExit,
 
-    SettingsCustomEventSceneEventsStart
+    SettingsCustomEventMatterCommStart,
+    SettingsCustomEventMatterCommComplete,
+    SettingsCustomEventMatterCommFail,
+
+    SettingsCustomEventRequiredWifiNotAvailable,
+
+    SettingsCustomEventSceneEventsStart,
 } SettingsCustomEvent;
 
 typedef struct SettingsApp {
@@ -28,6 +37,10 @@ typedef struct SettingsApp {
     FrontDisplaySrv* front_display;
     BackDisplaySrv* back_display;
 
+    MatterSrv* matter;
+    FuriPubSubSubscription* matter_subscription;
+    WifiPoller* wifi;
+
     Widget* front_scene_window;
 
     FlexLayout* back_container;
@@ -40,3 +53,5 @@ void settings_send_custom_event(SettingsApp* instance, uint32_t event);
 void settings_push_location(SettingsApp* instance, const char* location_name);
 
 void settings_pop_location(SettingsApp* instance);
+
+bool settings_check_wifi_connectivity(SettingsApp* instance);
