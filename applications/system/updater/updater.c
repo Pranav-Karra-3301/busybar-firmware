99efa716008f87797b2ecde744542ab2d17572e5
Ivan Shestakov <vanyww2@gmail.com>
2026-01-30 22:22:48 +0400

    [FW-547] Updater: Enable Background Auto-Update (#416)
    * Make autoupdates enabled by default
    
    * Make clock application pause autoupdates
    
    * Make busy application pause autoupdates while timer is running
    
    * Make autoupdates run only if NVM debug flag is reset
    
    ---------
    
    Co-authored-by: hedger <hedger@users.noreply.github.com>

--- applications/system/updater/updater.c
+++ applications/system/updater/updater.c
@@ -214,6 +214,11 @@ static void autoupdate_timer_callback(void* context) {
         return;
     }
 
+    if(furi_hal_nvm_is_flag_set(FuriHalNvmFlagDebug)) {
+        FURI_LOG_D(TAG, "Autoupdate: skipped, debug is enabled");
+        return;
+    }
+
     Sntp* sntp = furi_record_open(RECORD_SNTP);
     LocalTime local_time = sntp_get_local_time(sntp);
     furi_record_close(RECORD_SNTP);
