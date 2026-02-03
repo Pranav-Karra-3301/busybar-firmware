f6ab9a2b4c39286a47ea2382db12baa63754c310
Georgii Surkov <37121527+gsurkov@users.noreply.github.com>
2026-02-03 09:30:20 +0000

    [FW-492] MQTT Service refactoring (#434)
    * Mqtt: refactor api message handling
    
    * Mqtt: refactor constructor code
    
    * Mqtt: refactor settings and profiles
    
    * Mqtt: separate event handlers, improve const correctness
    
    * Mqtt: fix custom_url valid condition
    
    * Mqtt: refactor saved state
    
    * Mqtt: remove redundant state variable
    
    * Mqtt: improve event syntax
    
    * Mqtt: improve pub/sub logic
    
    * Mqtt: initial subscribe/unsubscribe api implementation, port BusyTimer
    
    * Mqtt: move api handlers to a separate file
    
    * Mqtt: move connection handlers to a separate file, rework api handlers
    
    * Mqtt: remove topic callback parameter
    
    * Mqtt: make subscription callback API more abstract
    
    * Mqtt: better api message handler names
    
    * Mqtt: extend publish_internal
    
    * Mqtt: refactor screen streaming
    
    * Mqtt: rearrange filex, add comments
    
    * Mqtt: remove redundant member variables
    
    * Mqtt: initial Mqtt Http API refactoring
    
    * Mqtt: intermediate Mqtt HTTP proxy implementation
    
    * Mqtt: implement publishing with properties
    
    * Mqtt: fix response topic
    
    * Mqtt: reduce HTTP proxy stack size
    
    * Mqtt: refactor screen streaming to a permanent service
    
    * Mqtt: rename some files & types
    
    * Mqtt: remove timestamp parsing from screen streaming
    
    * Mqtt: rename streaming service once again
    
    * Mqtt: make private headers
    
    * Mqtt: increase mongoose poll timeout to 1000 ms everywhere
    
    * Mqtt: fixup log tags
    
    * Mqtt: separate account functions
    
    * Mqtt: rename service folder to mqtt
    
    * Mqtt: fixup include paths
    
    * Mqtt: rename mqtt_client.* to mqtt.*
    
    * Mqtt: rename all mentions of MqttClient to Mqtt
    
    * Mqtt: use unified log tag
    
    * Mqtt: improve property handling
    
    * Mqtt: tidy up code
    
    * Mqtt: only subscribe to relevant topics
    
    * MqttHttpProxy: consider requests with no response topic valid
    
    * Mqtt: unify state checking code
    
    * Mqtt: move message to separate file
    
    * Mqtt: move subscription handling to separate file
    
    * Mqtt: return trimmed response topic
    
    * Mqtt: do not use dynamic memory to match incoming topics
    
    * Mqtt: remove mongoose typedefs for consistency
    
    * Mqtt: implement message expiry interval property
    
    * MqttStreaming: add fallback expiry interval
    
    * Mqtt: add Doxygen comments in mqtt.h
    
    * Mqtt: return an error if publishing is impossible
    
    * Mqtt: add a special MqttStatus value
    
    * Mqtt: actually use the value returned by mqtt_publish_internal()
    
    * Mqtt: fix typo
    
    * Mqtt: properly free memory in all cases
    
    * Mqtt: return proper error code
    
    * Mqtt: check for non-NULL connection for good measure
    
    * MqttHttpProxy: rewrite request processing in a more obvious way
    
    * MqttHttpProxy: remove unused member variable
    
    * MqttHttpProxy: fix a typo
    
    * Revert "MqttHttpProxy: rewrite request processing in a more obvious way"
    
    This reverts commit fd93a6660cef714f4935281f9665483e372993a5.
    
    * MqttHttpProxy: handle possible connection failure to loopback
    
    * MqttHttpProxy: improve URI handling
    
    * MqttHttpProxy: further reduce data copying
    
    ---------
    
    Co-authored-by: hedger <hedger@users.noreply.github.com>

--- applications/services/mqtt/mqtt_tls_private.c
+++ applications/services/mqtt/mqtt_tls_private.c
@@ -1,4 +1,4 @@
-#include "mqtt_client_i.h"
+#include <mongoose.h>
 #include <mbedtls/ssl.h>
 #include <mbedtls/pk.h>
 #include <pk_wrap.h>
@@ -122,7 +122,7 @@ static bool tls_load_cert_from_file(char* path, mbedtls_x509_crt* crt) {
     do {
         if(!storage_file_open(file, path, FSAM_READ, FSOM_OPEN_EXISTING)) {
             FURI_LOG_E(TAG, "Cert file error: %s", storage_file_get_error_desc(file));
-            return false;
+            break;
         }
 
         cert_len = storage_file_size(file);
@@ -130,26 +130,27 @@ static bool tls_load_cert_from_file(char* path, mbedtls_x509_crt* crt) {
 
         if(storage_file_read(file, cert_buf, cert_len) != cert_len) {
             FURI_LOG_E(TAG, "Cert file read error");
-            return false;
+            break;
         }
+
+        const int parse_result = mbedtls_x509_crt_parse(crt, cert_buf, cert_len + 1);
+
+        if(parse_result != 0) {
+            FURI_LOG_E(TAG, "Cert parse error -0x%04X", -parse_result);
+            break;
+        }
+
         success = true;
     } while(0);
 
-    storage_file_close(file);
+    if(cert_buf) {
+        free(cert_buf);
+    }
+
+    storage_file_free(file);
     furi_record_close(RECORD_STORAGE);
 
-    if(!success) {
-        if(cert_buf) free(cert_buf);
-        return false;
-    }
-
-    int ret = mbedtls_x509_crt_parse(crt, cert_buf, cert_len + 1);
-    free(cert_buf);
-    if(ret != 0) {
-        FURI_LOG_E(TAG, "Cert parse error -0x%04X", -ret);
-        return false;
-    }
-    return true;
+    return success;
 }
 
 static bool tls_load_key_from_file(char* path, mbedtls_pk_context* pk) {
@@ -203,7 +204,7 @@ static int tls_net_send(void* ctx, const unsigned char* buf, size_t len) {
 
 static int tls_net_recv(void* ctx, unsigned char* buf, size_t len) {
     long n = mg_io_recv((struct mg_connection*)ctx, buf, len);
-    if(n == MG_IO_WAIT) return MBEDTLS_ERR_SSL_WANT_WRITE;
+    if(n == MG_IO_WAIT) return MBEDTLS_ERR_SSL_WANT_READ;
     if(n == MG_IO_RESET) return MBEDTLS_ERR_NET_CONN_RESET;
     if(n == MG_IO_ERR) return MBEDTLS_ERR_NET_RECV_FAILED;
     return (int)n;
