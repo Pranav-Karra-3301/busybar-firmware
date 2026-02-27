942a293ec02775e09ed42092073bda5b9fe3d39b
Nikolay Minaylov <nm29719@gmail.com>
2026-02-27 22:35:19 +0300

    Intercom deadlock fix in 917_info (#504)
    * Fix intercom deadlock in 917_info
    
    * Intercom channel ready wait
    
    ---------
    
    Co-authored-by: hedger <hedger@users.noreply.github.com>

--- applications/services/si917_info/si917_info_client.c
+++ applications/services/si917_info/si917_info_client.c
@@ -23,9 +23,7 @@ static void si917_info_client_rx_callback(const void* data, size_t data_size, vo
     furi_assert(context);
     Si917InfoClient* instance = context;
 
-    furi_check(
-        furi_message_queue_put(instance->response_queue, &msg->data, FuriWaitForever) ==
-        FuriStatusOk);
+    furi_message_queue_put(instance->response_queue, &msg->data, 0);
 }
 
 static Si917InfoClient* si917_info_client_alloc(void) {
@@ -37,6 +35,7 @@ static Si917InfoClient* si917_info_client_alloc(void) {
     client->intercom = furi_record_open(RECORD_INTERCOM);
     client->intercom_ch = intercom_channel_open(
         client->intercom, IntercomChannelIdSi917Info, si917_info_client_rx_callback, client);
+    intercom_channel_await_peer_ready(client->intercom_ch, FuriWaitForever);
 
     return client;
 }
@@ -51,6 +50,8 @@ bool si917_info_get(Si917InfoClient* client, Si917InfoData* info) {
         memcpy(info, &client->info_data, sizeof(Si917InfoData));
         success = true;
     } else {
+        furi_message_queue_reset(client->response_queue);
+
         Si917InfoRequestMessage request_msg = {.type = Si917InfoMessageRequest};
 
         size_t packet_len = sizeof(Si917InfoRequestMessage);
@@ -63,7 +64,7 @@ bool si917_info_get(Si917InfoClient* client, Si917InfoData* info) {
             memcpy(info, &client->info_data, sizeof(Si917InfoData));
             success = true;
         } else {
-            FURI_LOG_E(TAG, "Request timeout");
+            FURI_LOG_E(TAG, "Response timeout");
         }
 
         client->data_valid = success;
