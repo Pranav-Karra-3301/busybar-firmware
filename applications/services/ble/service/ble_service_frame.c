31043b346712fde4194fe835f107590c25d1dbfe
RebornedBrain <RebornedBrain@gmail.com>
2026-04-06 12:24:43 +0300

    Frame logic adjusted and comments added

--- applications/services/ble/service/ble_service_frame.c
+++ applications/services/ble/service/ble_service_frame.c
@@ -1,5 +1,8 @@
 #include "ble_service_frame.h"
 #include "../ble_intercom_types.h"
+#include "../ble_common.h"
+
+#define TAG "BleSrvFrame"
 
 /**
  * @brief This must be less than @ref INTERCOM_SYNC_CHAR_TIMEOUT_MS.
@@ -11,7 +14,21 @@ struct BleServiceFrame {
     FuriSemaphore* lock;
     size_t size;
     uint8_t* buf;
+    uint8_t* free_offset;
 };
+
+static void ble_service_frame_check_resize(BleServiceFrame* instance, size_t new_data_size) {
+    furi_assert(instance);
+    const size_t current_data_size = (instance->free_offset - instance->buf);
+    const size_t desired_size = current_data_size + new_data_size;
+    furi_assert(desired_size < MAX_BLE_INTERCOM_FRAME_SIZE);
+
+    if(desired_size > instance->size) {
+        instance->buf = realloc(instance->buf, desired_size);
+        instance->free_offset = instance->buf + current_data_size;
+        instance->size = desired_size;
+    }
+}
 
 BleServiceFrame* ble_service_frame_alloc() {
     BleServiceFrame* instance = malloc(sizeof(BleServiceFrame));
@@ -23,41 +40,17 @@ BleServiceFrame* ble_service_frame_alloc() {
 void ble_service_frame_free(BleServiceFrame* instance) {
     furi_assert(instance);
     furi_semaphore_free(instance->lock);
+    instance->free_offset = NULL;
+    free(instance->buf);
     free(instance);
-}
-
-bool ble_service_frame_pending(BleServiceFrame* instance) {
-    furi_assert(instance);
-    return instance->pending;
-}
-
-bool ble_service_frame_put_data(BleServiceFrame* instance, const void* data, size_t size) {
-    furi_assert(instance);
-    furi_assert(data);
-    bool result = false;
-    if(ble_service_frame_lock(instance)) {
-        ble_service_frame_check_resize(instance, size);
-        memcpy(instance->buf, data, size);
-        result = true;
-    }
-    return result;
-}
-
-void* ble_service_frame_get_data_ptr(BleServiceFrame* instance) {
-    furi_assert(instance);
-    return instance->buf;
 }
 
 bool ble_service_frame_lock(BleServiceFrame* instance) {
     furi_assert(instance);
     instance->pending = furi_semaphore_acquire(
                             instance->lock, BLE_SERVICE_INPUT_FRAME_LOCK_TIMEOUT) == FuriStatusOk;
-    // if(furi_semaphore_acquire(instance->lock, BLE_SERVICE_INPUT_FRAME_LOCK_TIMEOUT) !=
-    //    FuriStatusOk) {
-    //     return false;
-    // }
-    // instance->pending = true;
-    // return true;
+
+    if(!instance->pending) BLE_LOG_W("Failed to lock frame");
     return instance->pending;
 }
 
@@ -65,18 +58,30 @@ void ble_service_frame_unlock(BleServiceFrame* instance) {
     furi_assert(instance);
     memset(instance->buf, 0, instance->size);
     instance->pending = false;
-    if(furi_semaphore_release(instance->lock) != FuriStatusOk) {
-        // BLE_LOG_W("%s - frame unlock failed", instance->config->name);
-    }
+    instance->free_offset = instance->buf;
+    furi_semaphore_release(instance->lock);
 }
 
-void ble_service_frame_check_resize(BleServiceFrame* instance, size_t new_frame_size) {
+bool ble_service_frame_pending(BleServiceFrame* instance) {
     furi_assert(instance);
-    furi_assert(new_frame_size < MAX_BLE_INTERCOM_FRAME_SIZE);
+    return instance->pending;
+}
 
-    if(new_frame_size > instance->size) {
-        instance->buf = realloc(instance->buf, new_frame_size);
-        instance->size = new_frame_size;
-        // BLE_LOG_D("%s - buf_size: %d", instance->config->name, new_frame_size);
-    }
+size_t ble_service_frame_get_data_size(BleServiceFrame* instance) {
+    furi_assert(instance);
+    return instance->free_offset - instance->buf;
+}
+
+const void* ble_service_frame_get_data_ptr(BleServiceFrame* instance) {
+    furi_assert(instance);
+    return instance->buf;
+}
+
+void ble_service_frame_append_data(BleServiceFrame* instance, const void* data, size_t size) {
+    furi_assert(instance);
+    furi_assert(data);
+
+    ble_service_frame_check_resize(instance, size);
+    memcpy(instance->free_offset, data, size);
+    instance->free_offset += size;
 }
