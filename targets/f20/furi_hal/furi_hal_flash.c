ec8797b8b411bf6da416ce0b387b5d573a1445c8
Lesha Lomalkin <flp16@lomalkin.org>
2026-04-08 21:15:49 +0100

    otp: LL_ICACHE_Invalidate after flash

--- targets/f20/furi_hal/furi_hal_flash.c
+++ targets/f20/furi_hal/furi_hal_flash.c
@@ -10,6 +10,7 @@
 #include <furi_hal_bits.h>
 
 #include "stm32u5xx.h"
+#include "stm32u5xx_ll_icache.h"
 
 #define FURI_HAL_FLASH_TOTAL_PAGES (2 * FLASH_PAGE_NB)
 #define FURI_HAL_FLASH_BUSY_WAIT_TIMEOUT_US \
@@ -294,6 +295,8 @@ bool furi_hal_flash_program_otp(const uint32_t base, const uint8_t* data, uint16
     FLASH->NSCR &= ~(FLASH_NSCR_BWR | FLASH_NSCR_PG);
 
     furi_hal_flash_lock();
+
+    LL_ICACHE_Invalidate();
     FURI_CRITICAL_EXIT();
 
     return true;
