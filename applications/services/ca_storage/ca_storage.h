0830ac42b02a3d823982c13153a5d26b54f022d8
Georgii Surkov <georgii.surkov@outlook.com>
2026-05-08 20:55:39 +0100

    CaStorage: add more docstrings

--- applications/services/ca_storage/ca_storage.h
+++ applications/services/ca_storage/ca_storage.h
@@ -1,5 +1,14 @@
+/**
+ * @file ca_storage.h
+ * @brief Certificate Authority (CA) storage API.
+ */
 #pragma once
 
+/**
+ * @brief The string key for CaStorage instance access
+ *
+ * Get the instance pointer by calling `furi_record_open(RECORD_CA_STORAGE);`
+ */
 #define RECORD_CA_STORAGE "ca_storage"
 
 typedef struct CaStorage CaStorage;
@@ -10,7 +19,7 @@ typedef struct CaStorage CaStorage;
  * @note The return value is guaranteed to be valid throughout the firmware's lifetime
  *       and the string it is pointing to is guaranteed to be zero-terminated.
  *
- * @param[in] instance
+ * @param[in] instance Pointer the the CaStorage instance
  * @returns pointer to the CA bundle string
  */
 const char* ca_storage_get_pem_bundle(CaStorage* instance);
