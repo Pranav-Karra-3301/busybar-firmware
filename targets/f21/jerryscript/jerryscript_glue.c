d7e34ec2bbb11fdbbb7f896a2172a4fdc9b9a82a
Ivan Mikhailov <imihajlow@gmail.com>
2026-06-30 12:03:14 +0200

    Jerryscript glue time functions

--- targets/f21/jerryscript/jerryscript_glue.c
+++ targets/f21/jerryscript/jerryscript_glue.c
@@ -1,11 +1,16 @@
 #include <furi/furi.h>
-#include <jerryscript.h>
+#include <time/time.h>
 #include <thread_local_config.h>
 
+#include <utz/utz.h>
+#include <jerryscript.h>
+
+#define TAG       "JSGlue"
 #define HEAP_SIZE 8192
 
 size_t jerry_port_context_alloc(size_t context_size) {
-    void* tlp = furi_thread_local_storage_pointer_get(NULL, ThreadLocalStoragePointerIdJerryscript);
+    void* tlp =
+        furi_thread_local_storage_pointer_get(NULL, ThreadLocalStoragePointerIdJerryscript);
     furi_check(tlp == NULL);
     size_t alloc_size = context_size + HEAP_SIZE;
     tlp = malloc(alloc_size);
@@ -18,25 +23,31 @@ struct jerry_context_t* jerry_port_context_get(void) {
 }
 
 void jerry_port_context_free(void) {
-    void* tlp = furi_thread_local_storage_pointer_get(NULL, ThreadLocalStoragePointerIdJerryscript);
+    void* tlp =
+        furi_thread_local_storage_pointer_get(NULL, ThreadLocalStoragePointerIdJerryscript);
     free(tlp);
     furi_thread_local_storage_pointer_set(NULL, ThreadLocalStoragePointerIdJerryscript, NULL);
-}
-
-int32_t jerry_port_local_tza(double unix_ms) {
-    // TODO
-    UNUSED(unix_ms);
-    return 0;
 }
 
 void jerry_port_init(void) {
 }
 
 void jerry_port_fatal(jerry_fatal_code_t code) {
+    FURI_LOG_E(TAG, "jerryscript fatal error %d", code);
     furi_crash(code);
 }
 
 double jerry_port_current_time(void) {
-    // TODO
-    return 0.0;
+    return (double)time_get_timestamp_ms();
+}
+
+int32_t jerry_port_local_tza(double unix_ms) {
+    Time* time = furi_record_open(RECORD_TIME);
+    TimeSettings settings;
+    time_get_settings(time, &settings);
+    furi_record_close(RECORD_TIME);
+    DateTimeMs dt = datetime_timestamp_ms_to_datetime((time_t)unix_ms);
+    utz_offset_t offset;
+    utz_get_current_offset(&settings.timezone, &dt.dt, &offset);
+    return (offset.hours * 60 + offset.minutes) * 60 * 1000;
 }
