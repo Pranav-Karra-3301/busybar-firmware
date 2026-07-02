960d3656376e951ca24cf6b45ff4f0c4a8ccb8f6
Ivan Mikhailov <imihajlow@gmail.com>
2026-07-02 16:24:34 +0200

    Revert "Common enum for thread-local pointers"
    This reverts commit ec7be960f08e94222c9dd3a279b5d00dda3d449a.

--- targets/f21/lwip/lwip_glue.c
+++ targets/f21/lwip/lwip_glue.c
@@ -1,7 +1,5 @@
 #include <furi.h>
 
-#include <thread_local_config.h>
-
 #include <lwip/debug.h>
 #include <lwip/sys.h>
 #include <lwip/mem.h>
@@ -237,7 +235,7 @@ sys_thread_t
 
 // netconn per thread semaphores
 
-#define FURI_THREAD_LOCAL_SEM_INDEX ThreadLocalStoragePointerIdLwip
+#define FURI_THREAD_LOCAL_SEM_INDEX 0
 
 sys_sem_t* sys_arch_netconn_sem_get(void) {
     return furi_thread_local_storage_pointer_get(NULL, FURI_THREAD_LOCAL_SEM_INDEX);
