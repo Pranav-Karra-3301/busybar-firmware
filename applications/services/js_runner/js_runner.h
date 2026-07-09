3cb6e0f71b9785723d68263501f43ad39a35882d
Ivan Mikhailov <imihajlow@gmail.com>
2026-07-09 11:55:33 +0200

    Add doxygen

--- applications/services/js_runner/js_runner.h
+++ applications/services/js_runner/js_runner.h
@@ -24,17 +24,33 @@ typedef void (*JsRunnerConsoleWriteCallback)(
     size_t size,
     void* context);
 
-size_t js_runner_context_alloc(JsRunner* instance, size_t context_size);
-
-void js_runner_context_free(JsRunner* instance);
-
-void* js_runner_context_get(JsRunner* instance);
-
-void js_runner_get_root_path(JsRunner* instance, FuriString* path);
-
+/** @brief Run a JS application.
+ *
+ * This function blocks until the script terminates.
+ *
+ * @param path entry point script path.
+ * @param heap_size JS heap size for the app in bytes.
+ * @param console_write_cb callback function for JS console methods (console.log, console.error, console.info). Supply NULL to disable console.
+ * @param console_write_context user pointer passed to console_write_cb.
+ *
+ * @return error code
+ */
 JsRunnerError js_runner_run(
     JsRunner* instance,
     const char* path,
     size_t heap_size,
     JsRunnerConsoleWriteCallback console_write_cb,
     void* console_write_context);
+
+/** @brief Allocate Jerryscript context for current thread. This function is used by jerryscript glue. */
+size_t js_runner_context_alloc(JsRunner* instance, size_t context_size);
+
+/** @brief Free Jerryscript context for current thread. This function is used by jerryscript glue. */
+void js_runner_context_free(JsRunner* instance);
+
+/** @brief Get Jerryscript context for current thread. This function is used by jerryscript glue. */
+void* js_runner_context_get(JsRunner* instance);
+
+/** @brief Get root path of the current JS app (folder containg entry point).
+ * This function is used by jerryscript glue. */
+void js_runner_get_root_path(JsRunner* instance, FuriString* path);
