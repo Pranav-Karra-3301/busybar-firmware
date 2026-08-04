808beafe841b4bffe3774ecffd9c8ae297bafea2
Ivan Mikhailov <imihajlow@gmail.com>
2026-08-04 10:27:11 +0200

    JS: use utility functions

--- applications/services/js_runner/js_interval.c
+++ applications/services/js_runner/js_interval.c
@@ -142,17 +142,10 @@ static jerry_value_t clear_interval(
 void js_setup_interval_methods(void) {
     jerry_value_t global_obj = jerry_current_realm();
 
-    jerry_value_t set_interval_fn = jerry_function_external(set_interval);
-    jerry_value_t set_timeout_fn = jerry_function_external(set_timeout);
-    jerry_value_t clear_interval_fn = jerry_function_external(clear_interval);
+    js_set_method(global_obj, "setInterval", set_interval);
+    js_set_method(global_obj, "setTimeout", set_timeout);
+    js_set_method(global_obj, "clearInterval", clear_interval);
+    js_set_method(global_obj, "clearTimeout", clear_interval);
 
-    js_check_and_free(jerry_object_set_sz(global_obj, "setInterval", set_interval_fn));
-    js_check_and_free(jerry_object_set_sz(global_obj, "setTimeout", set_timeout_fn));
-    js_check_and_free(jerry_object_set_sz(global_obj, "clearInterval", clear_interval_fn));
-    js_check_and_free(jerry_object_set_sz(global_obj, "clearTimeout", clear_interval_fn));
-
-    jerry_value_free(set_interval_fn);
-    jerry_value_free(set_timeout_fn);
-    jerry_value_free(clear_interval_fn);
     jerry_value_free(global_obj);
 }
